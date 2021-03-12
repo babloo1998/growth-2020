8d0557fcc0669c58ce1103e8a3a09945017e9be8
Babloo Mishra <babloo.mishra@tothenew.com>
2021-03-12 14:47:00 +0530

    Updates

---
+++ precache-manifest.f2f582254a1dcbb187a174a501271532.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d8f894ae02a2b8abbba22d679cabdea",
+    "url": "/growth-2020/index.html"
+  },
+  {
+    "revision": "d7ecae923de07628d7aa",
+    "url": "/growth-2020/static/css/2.179b0f24.chunk.css"
+  },
+  {
+    "revision": "2b45d639688afa1d1adc",
+    "url": "/growth-2020/static/css/main.4a092277.chunk.css"
+  },
+  {
+    "revision": "d7ecae923de07628d7aa",
+    "url": "/growth-2020/static/js/2.b400389b.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "/growth-2020/static/js/2.b400389b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b45d639688afa1d1adc",
+    "url": "/growth-2020/static/js/main.2bb96e2d.chunk.js"
+  },
+  {
+    "revision": "f4b8d9d582502d122b5f",
+    "url": "/growth-2020/static/js/runtime-main.3374b9f8.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/growth-2020/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/growth-2020/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/growth-2020/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/growth-2020/static/media/slick.f97e3bbf.svg"
+  }
+]);
